68e6d0d47d3d2580e8bc206f6daea7dab0140896
xiaohe.wp <xiaohe.wp@alibaba-inc.com>
2018-06-12 14:21:51 +0800

    兼容React16

--- tests/Tooltip.spec.js
+++ tests/Tooltip.spec.js
@@ -1,17 +1,25 @@
-import expect from 'expect.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-dom/test-utils';
-import Enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-15'
-import Tooltip from '../src';
+import expect from "expect.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import TestUtils, { Simulate } from "react-dom/test-utils";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Tooltip from "../src";
 
 Enzyme.configure({
-    adapter: new Adapter()
-})
+  adapter: new Adapter()
+});
 
-describe('Tooltip', () => {
-    it('render', () => {
-        mount(<Tooltip placement="left" trigger={['click']} overlay={<span>tooltip</span>}><a href='#'>hover</a></Tooltip>)
-    })
+describe("Tooltip", () => {
+  it("render", () => {
+    mount(
+      <Tooltip
+        placement="left"
+        trigger={["click"]}
+        overlay={<span>tooltip</span>}
+      >
+        <a href="#">hover</a>
+      </Tooltip>
+    );
+  });
 });
