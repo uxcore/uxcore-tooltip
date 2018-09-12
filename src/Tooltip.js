ebcb0b871d19ce2ca8675f0d1cafee248cbc66ef
micate <micate@me.com>
2018-09-12 09:41:32 +0800

    support js style export

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,13 +1,15 @@
 import RcTooltip from 'rc-tooltip';
 
 export default class Tooltip extends RcTooltip {
-    static displayName = 'uxcore-tooltip'
-    static propTypes = {
-        ...RcTooltip.propTypes,
-    }
-    static defaultProps = {
-        ...RcTooltip.defaultProps,
-        prefixCls: 'kuma-tooltip',
-        transitionName: 'tip-slide',
-    }
+  static displayName = 'uxcore-tooltip';
+
+  static propTypes = {
+    ...RcTooltip.propTypes,
+  };
+
+  static defaultProps = {
+    ...RcTooltip.defaultProps,
+    prefixCls: 'kuma-tooltip',
+    transitionName: 'tip-slide',
+  };
 }
